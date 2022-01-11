c571e7e5b166b7b709e069780c3da8e073c58bcb
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-01-11 10:46:39 +0000

    add addNewItem() in order-summary.js

--- public/scripts/order-summary.js
+++ public/scripts/order-summary.js
@@ -17,3 +17,16 @@ $(() => {
     totalPlusTax = 0;
     $('#clear-cart').hide();
   };
+
+
+    // Adds row to the order summary table
+    const addNewItem = (itemName, itemQuantity, itemPrice) => {
+      const $selectedItem = $('<tr>').addClass('order-item');
+      const $itemName = $('<td>').addClass('order-item-name').text(itemName);
+      const $quantity = $('<td>').addClass('order-quantity').text(itemQuantity);
+      const $price = $('<td>').addClass('order-price').text(Number.parseFloat(itemPrice).toFixed(2));
+
+      $selectedItem.append($itemName, $quantity, $price);
+
+      return $selectedItem;
+    };
