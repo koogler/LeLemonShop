701ed9a7ac791c7a4d3ee1e5493d8ed7a76dc213
Desmond Parker <evan.parker95@gmail.com>
2022-01-11 17:10:12 +0000

    final seeds with at least 4 examples of each

--- db/schema/04_menu_orders.sql
+++ db/schema/04_menu_orders.sql
@@ -1,10 +1,9 @@
-DROP TABLE IF EXISTS order_contents CASCADE;
+DROP TABLE IF EXISTS menu_orders CASCADE;
 DROP TABLE IF EXISTS full_ordered;
 
-CREATE TABLE order_contents (
+CREATE TABLE menu_orders (
   id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id)
   food_id INTEGER REFERENCES food_item(id),
   quantity SMALLINT DEFAULT 0,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  note TEXT
 );
--- db/seeds/02_food_items_seeds.sql
+++ db/seeds/02_food_items_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO food_item (name, image, description, price)
 VALUES
-('Raspberry Lemonade', 'www.exampleexample.com', 'descript', 700),
-('Strawberry Lemonade', 'www.lemonbatter.com', 'descript', 650),
-('Sweet Tea', 'www.lemonbatter.com', 'descript', 650),
-('Chocolate Chip Cookie', 'www.lemonbatter.com', 'descript', 650)
+('Raspberry Lemonade', 'www.exampleexample.com', 'Our famous lemondade with a twist of raspberry.', 700),
+('Strawberry Lemonade', 'www.lemonbatter.com', 'Our famous lemondade with a twist of Strawberry.', 650),
+('Sweet Tea', 'www.lemonbatter.com', "Our amazing sweet tea, if you aren't in the mood for lemonade.", 650),
+('Chocolate Chip Cookie', 'www.lemonbatter.com', 'Fresh chocolate chip cookies, made fresh every morning.', 650)
