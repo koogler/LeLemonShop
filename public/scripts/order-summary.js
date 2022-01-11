5ff9201ed230fea3ccf640d16e7c13faa9b77e37
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-01-11 11:09:09 +0000

    add final changes in order-summary.js

--- public/scripts/order-summary.js
+++ public/scripts/order-summary.js
@@ -18,67 +18,67 @@ $(() => {
     $('#clear-cart').hide();
   };
 
+  // Adds row to the order summary table
+  const addNewItem = (itemName, itemQuantity, itemPrice) => {
+    const $selectedItem = $('<tr>').addClass('order-item');
+    const $itemName = $('<td>').addClass('order-item-name').text(itemName);
+    const $quantity = $('<td>').addClass('order-quantity').text(itemQuantity);
+    const $price = $('<td>').addClass('order-price').text(Number.parseFloat(itemPrice).toFixed(2));
 
-    // Adds row to the order summary table
-    const addNewItem = (itemName, itemQuantity, itemPrice) => {
-      const $selectedItem = $('<tr>').addClass('order-item');
-      const $itemName = $('<td>').addClass('order-item-name').text(itemName);
-      const $quantity = $('<td>').addClass('order-quantity').text(itemQuantity);
-      const $price = $('<td>').addClass('order-price').text(Number.parseFloat(itemPrice).toFixed(2));
+    $selectedItem.append($itemName, $quantity, $price);
 
-      $selectedItem.append($itemName, $quantity, $price);
-
-      return $selectedItem;
-    };
-
-
-    const refreshSumOrder = (total) => {
-      const tax = Math.round(total * 15) / 100;
-
-      $sumOrder.empty();
-
-      const $markup = `
-        <tr class="order-item">
-          <td class="order-item-name">---</td>
-          <td class="order-quantity"></td>
-          <td class="order-price"></td>
-        </tr>
-        <tr class="order-item">
-          <td class="order-item-name">Subtotal</td>
-          <td class="order-quantity"></td>
-          <td class="order-price">${Number.parseFloat(total).toFixed(2)}</td>
-        </tr>
-        <tr class="order-item">
-          <td class="order-item-name">Total Tax (15%)</td>
-          <td class="order-quantity"></td>
-          <td class="order-price">${tax}</td>
-        </tr>
-
-        <tr class="order-item">
-          <td class="order-item-name"><strong>Total</strong></td>
-          <td class="order-quantity"></td>
-          <td class="order-price"><strong>${Math.round((total + tax) * 100) / 100}</strong></td>
-        </tr>
-        `
-      $sumOrder.append($markup);
-      return $sumOrder;
-    };
+    return $selectedItem;
+  };
 
 
-    const renderItems = () => {
-      $itemContainer.empty();
-      let $selectedItem;
-      for (const item of orderList.items) {
-        $selectedItem = addNewItem(item.name, item.quantity, item.price);
-        $itemContainer.append($selectedItem);
-      }
-    };
+  const refreshSumOrder = (total) => {
+    const tax = Math.round(total * 15) / 100;
 
-    //Calling function to populate items when object has content
-    renderItems();
+    $sumOrder.empty();
+
+    const $markup = `
+      <tr class="order-item">
+        <td class="order-item-name">---</td>
+        <td class="order-quantity"></td>
+        <td class="order-price"></td>
+      </tr>
+      <tr class="order-item">
+        <td class="order-item-name">Subtotal</td>
+        <td class="order-quantity"></td>
+        <td class="order-price">${Number.parseFloat(total).toFixed(2)}</td>
+      </tr>
+      <tr class="order-item">
+        <td class="order-item-name">Total Tax (15%)</td>
+        <td class="order-quantity"></td>
+        <td class="order-price">${tax}</td>
+      </tr>
+
+      <tr class="order-item">
+        <td class="order-item-name"><strong>Total</strong></td>
+        <td class="order-quantity"></td>
+        <td class="order-price"><strong>${Math.round((total + tax) * 100) / 100}</strong></td>
+      </tr>
+      `
+    $sumOrder.append($markup);
+    return $sumOrder;
+  };
 
 
-    // Checks if the item is in cart for changeCart() function)
+  const renderItems = () => {
+    $itemContainer.empty();
+    let $selectedItem;
+    for (const item of orderList.items) {
+      $selectedItem = addNewItem(item.name, item.quantity, item.price);
+      $itemContainer.append($selectedItem);
+    }
+  };
+
+  //Calling function to populate items when object has content
+  renderItems();
+
+
+
+  // Checks if the item is in cart for changeCart() function)
   const isInCart = (itemId) => {
     const itemsInCart = [];
     for(const item of orderList.items) {
@@ -101,7 +101,6 @@ $(() => {
   };
 
 
-
   // Makes changes in the cart, for the .ajax post
   const changeCart = (itemId, itemName, itemPrice) => {
     if (isInCart(itemId)) {
@@ -115,14 +114,6 @@ $(() => {
       });
     }
   };
-
-
-  $(document).on("click", "#clear-cart", function(event) {
-    event.preventDefault();
-    $itemContainer.empty();
-    $sumOrder.empty();
-    emptyCart();
-  });
 
 
   // When client clicks on the add-to-cart button
@@ -153,6 +144,15 @@ $(() => {
         console.log(`Error details: ${err}`);
       }
     });
+  });
+
+
+  $(document).on("click", "#clear-cart", function(event) {
+    event.preventDefault();
+
+    $itemContainer.empty();
+    $sumOrder.empty();
+    emptyCart();
   });
 
 
