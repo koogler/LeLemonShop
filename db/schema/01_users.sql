354343b45cfd5c3161623f36bd69017eadaf3973
Ren <cassandra.chen@uoit.net>
2022-01-12 16:23:08 +0000

    commit

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -7,9 +7,5 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   phone VARCHAR(255) NOT NULL,
-<<<<<<< HEAD
-  password VARCHAR (255) NOT NULL
-=======
   password VARCHAR(255) NOT NULL
->>>>>>> ren-route
 );
