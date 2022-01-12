127acffdb0e5a02685efeeb4047aa7b470607e52
Ren <cassandra.chen@uoit.net>
2022-01-12 00:33:00 +0000

    create post requests - incomplete

--- routes/orders.js
+++ routes/orders.js
@@ -18,12 +18,33 @@ module.exports = (db) => {
 
     db.query(query, qp)
       .then(data => {
-        const items = data.rows[qp];
+        const items = data.rows;
         res.json({items});
       })
       .catch(err => {
         res.status(500).send("No Lemons here");
       });
   });
+
+  router.post("/:id", (req, res) => {
+    const orderID = req.params.id;
+    const qp = [orderID]
+    let query = `
+    SELECT food_item.name as name, ROUND(SUM(food_item.price * quantity), 2) as total_price
+    FROM orders
+    JOIN food_item ON food_item.id = food_id
+    WHERE orders.id = $1
+    GROUP BY food_item.name;`;
+
+    db.query(query, qp)
+      .then(data => {
+        const items = data.rows;
+        res.json({items});
+      })
+      .catch(err => {
+        res.status(500).send("I've stapled the lemons to the wall");
+      });
+  });
+
   return router;
 };
