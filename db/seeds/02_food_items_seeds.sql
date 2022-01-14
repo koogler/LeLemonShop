02ab24f415f732ed8f2d4ed7980f73b32d59d31c
Desmond Parker <evan.parker95@gmail.com>
2022-01-14 15:11:08 +0000

    fixing broken URLs in seedds

--- db/seeds/02_food_items_seeds.sql
+++ db/seeds/02_food_items_seeds.sql
@@ -1,14 +1,10 @@
-
--- Current items on menu
-
 INSERT INTO food_items (name, image, description, price)
 VALUES
-('Raspberry Lemonade', 'https://images.pexels.com/photos/2110927/pexels-photo-2110927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Our famous lemonade with a twist of raspberry.', 700),
-('Strawberry Lemonade', 'https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80', 'Our famous lemonade with a twist of Strawberry.', 700),
-('Sweet Tea', 'www.lemonbatter.com', 'Our amazing sweet tea, if you are not in the mood for lemonade.', 750),
-('Chocolate Chip Cookie', 'https://images.unsplash.com/photo-1625876981820-be17a6807189?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', 'Fresh chocolate chip cookies, made fresh every morning.', 200),
-('Premium Lemonade', 'https://images.unsplash.com/photo-1507281549113-040fcfef650e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 'Our specialty lemonade, made with the finest lemons you can find in Canada!', 575),
-('Hallows Brew', 'https://cdn.pixabay.com/photo/2020/10/27/12/32/drink-5690582_960_720.jpg', 'Intense halloween drinks if you are bold enough', 800),
-('Sour Skids', 'https://pixabay.com/photos/gin-tonic-cocktail-lemon-alcohol-6875377/', 'Ready for something not-so-sweet? Extra lemon, less sugar is what you seek', 600),
-('Marshmallows Eve', 'https://pixabay.com/photos/rodessa-rozhdesvensky-holiday-4672690/', 'A monster drink with blended sweets, lemonade, and marshmallows', 900);
-
+('Raspberry Lemonade', 'https://images.pexels.com/photos/2110927/pexels-photo-2110927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Our famous lemonade with a twist of raspberry.', 7),
+('Strawberry Lemonade', 'https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80', 'Our famous lemonade with a twist of Strawberry.', 7),
+('Sweet Tea', 'https://images.pexels.com/photos/5455663/pexels-photo-5455663.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Our amazing sweet tea, if you are not in the mood for lemonade.', 7),
+('Chocolate Chip Cookie', 'https://images.unsplash.com/photo-1625876981820-be17a6807189?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', 'Fresh chocolate chip cookies, made fresh every morning.', 2),
+('Premium Lemonade', 'https://images.unsplash.com/photo-1507281549113-040fcfef650e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 'Our specialty lemonade, made with the finest lemons you can find in Canada!', 5),
+('Hallows Brew', 'https://cdn.pixabay.com/photo/2020/10/27/12/32/drink-5690582_960_720.jpg', 'Intense halloween drinks if you are bold enough', 8),
+('Sour Skids', 'https://cdn.pixabay.com/photo/2021/12/16/22/54/gin-tonic-6875377_960_720.jpg', 'Ready for something not-so-sweet? Extra lemon, less sugar is what you seek', 6),
+('Marshmallows Eve', 'https://cdn.pixabay.com/photo/2019/12/04/13/15/rodessa-4672690_960_720.jpg', 'A monster drink with blended sweets, lemonade, and marshmallows', 9);
